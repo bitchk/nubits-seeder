8dbd5adb687d4c74796fcb3dced452bd79721f09
Willy <pussinbootsdev@gmail.com>
2015-09-11 20:47:13 +0200

    various bug fixes 	geändert:               cf-php/cf.php

--- cf-php/cf.php
+++ cf-php/cf.php
@@ -2,7 +2,7 @@
 
 $domain ="domain.com";
 $name = "nuseed"; //subdomain e.g. name.domain.com 
-$number_of_records = 10; //maximum n A records with $name... 
+$number_of_records = 10; //maximum n A records with $name... 10 recommended 
 $user = "emailofcloudflareaccount"; //user name
 $key = "yourapikey"; //key for cloudflare api found in account settings
 $seed_dump = "/path/to/dnsseed.dump"; //absolute path to dnsseed.dump in the nubits-seeder root directory 
@@ -23,6 +23,10 @@ $i4 = 0; //counter
 $i3 = 0; //counter
 $i2 = 1; //counter, 1 (line 0 is a header)
 $i = 0; //counter
+$offset = 0; // 
+$ips_used = 0; //
+$i_offset = 0; //
+$end = false; //
 $ip_array=array();
 
 if (!file_exists($seed_dump))
@@ -35,7 +39,7 @@ else{
 	$response_object = $cf->rec_load_all($domain); //ALL dns entries of domain
 	$response_json=json_encode($response_object); //make objects to JSON
 	$response_array=json_decode($response_json, true); //make JSON to array, thanks php
-
+	
 	if(isset($response_array["err_code"]))
 	{
 		if($response_array["err_code"]=="E_UNAUTH")
@@ -69,10 +73,14 @@ else{
 		
 		$i2++;
 	}
+	
+// how many ips in list? are ips available? 
+	$ip_array_available = count ($ip_array);
+	if ($ip_array_available>0) {$ips_available=true;} else {$ips_available=false;} 
 
 //check for number of CF $name.$domain entries
 
-	$number_of_name_domain_records = count ($response_array["response"]["recs"]["objs"]); //count dns entries with $name.$domain
+	$number_of_name_domain_records = count($response_array["response"]["recs"]["objs"]); //count dns entries with $name.$domain
 	$entries=0;  //entry counter
 	while ($i3 < $number_of_name_domain_records)
 	{
@@ -82,7 +90,7 @@ else{
 		 }
 	$i3++;
 	}
-	
+
 //create needed entries if $entries differs from $number_of_records (initial create of zone file)
 
 	$difference=$number_of_records-$entries;
@@ -101,7 +109,7 @@ else{
 		$new_response_array=get_object_vars($write_new); 
 		$new_response_array_retry=$new_response_array;
 		
-		while($new_response_array_retry["msg"]=="The record already exists.")
+		while($new_response_array_retry["msg"]=="The record already exists." && $end==false)
 		{
 				$i5++; //move file pointer +1
 				$ip_new_retry=$ip_array[$i5]["ip"];
@@ -112,7 +120,10 @@ else{
 					$content_new_retry=$ip_new_retry;
 					$write_new_retry = $cf->rec_new($domain, $type, $name, $content_new_retry);
 					}
-				
+				else {
+					//seems like no more good ips are available 
+					$end=true;					
+					}
 				$new_response_array_retry=get_object_vars($write_new_retry); 
 		
 		}
@@ -120,22 +131,51 @@ else{
 		$i5++; //move file pointer!		
 		$i4++; //check next missing element
 	}
-	 
 	
-//make CF edits
-	while ($i < $number_of_records)
+//to many entries? delete!
+
+	while($difference < 0)
 	{
-		$id=$response_array["response"]["recs"]["objs"][$i]["rec_id"]; //get the DNS table row IDs 
+	$diff_rev=($difference*-1)+$offset;
+	$id_diff=$response_array["response"]["recs"]["objs"][$diff_rev]["rec_id"];
+	
+	if($response_array["response"]["recs"]["objs"][$diff_rev]["type"]==$type && $response_array["response"]["recs"]["objs"][$diff_rev]["name"] == "$name.$domain")
+	{
+		$delete=$cf->delete_dns_record($domain, $id_diff);
+		$difference++;
+	}
+	else {
+		$offset++;
+		}
+	}
+	 
 
-		$ip_edit=$ip_array[$i]["ip"];
-		$good_edit=$ip_array[$i]["good"];
-
+//make CF edits
+$end = false; 
+	while ($i<=$number_of_records && $end==false)
+	{
+		$number_of_records=$number_of_records-$number_of_records_solved;	
+				
+		$id=$response_array["response"]["recs"]["objs"][$i+$i_offset]["rec_id"]; //get the DNS table row 
+		 
+		$ip_edit=$ip_array[$ips_used]["ip"];
+		$good_edit=$ip_array[$ips_used]["good"];
 		if ($good_edit == 1 && $response_array["response"]["recs"]["objs"][$i]["type"]==$type && $response_array["response"]["recs"]["objs"][$i]["name"] == "$name.$domain") //only re-write the dns entry if good == 1 and type = A and domain name = $name.$domain
-			{
-			$content_edit=$ip_edit;
+			{	
+			$content_edit = $ip_edit;
 			$write_edit = $cf->rec_edit($domain, $type, $id, $name, $content_edit);
+			$number_of_records_solved++; //one less to edit
+			$ips_used++; //move ip list pointer
+			$i_offset++; //move $i_offset
 			}
-		$i++;
+		else {
+			if ($ips_used>$ip_array_available)	//no more IPs left in list 		
+				{ 
+				$end=true;
+				}
+			
+			$i++;
+			}
 	}
 }
 ?>
